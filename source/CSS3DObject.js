ee03c6185360e83a1aa2f67c5769882898ef4dbd
jaune <JauneLaCouleur@gmail.com>
2014-02-17 00:52:10 +0100

    use uglifyjs

--- source/CSS3DObject.js
+++
@@ -1,9 +0,0 @@
-
-var CSS3DObject = function (element) {
-
-    Object3D.call(this);
-
-    this.element = element;
-    this.element.style.position = 'absolute';
-};
-CSS3DObject.prototype = Object.create(Object3D.prototype);
--- source/CSS3DSprite.js
+++
@@ -1,5 +0,0 @@
-
-var CSS3DSprite = function (element) {
-    CSS3DObject.call(this, element);
-};
-CSS3DSprite.prototype = Object.create(CSS3DObject.prototype);
